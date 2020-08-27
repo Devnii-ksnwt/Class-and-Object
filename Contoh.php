2abb4226c8c4789587e4ef48c8873f5b75f72de8
Devnii-ksnwt <65270284+Devnii-ksnwt@users.noreply.github.com>
2020-08-27 09:07:28 +0700

    Add files via upload

---
+++ Contoh.php
@@ -0,0 +1,17 @@
+<?php 
+class hari{
+
+}
+$a = new hari();
+$b = new hari();
+$c = new hari();
+$d = new hari();
+$e = new hari();
+
+echo var_dump($a);
+echo var_dump($b);
+echo var_dump($c);
+echo var_dump($d);
+echo var_dump($e);
+?>
+
